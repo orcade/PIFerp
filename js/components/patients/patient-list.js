b901d39930045e5a896d3f7d509b7d6055a06616
orcade <51912585+orcade@users.noreply.github.com>
2019-07-26 16:08:53 +0200

    code css a recup
    verification code css a ajouter a vue js

--- js/components/patients/patient-list.js
+++ js/components/patients/patient-list.js
@@ -12,45 +12,51 @@ const PatientList = {
     <div v-if="error" class="error">
         {{ error }}
     </div>
+    <div class="rectangle">
 
-    <button class="add">
-    <router-link class="add"  to=/patients/patient-add>Ajouter patient</router-link>
-    </button>
+    <button class="add"  v-on:click="sendModif">Ajouter</button>
+    <button class="search"  v-on:click="fetchData">Rechercher</button>
+<div class="form">
+    <div>
+        <label class="entree">Prénom</label>
+        <input class="champs" type="text" v-model="item.prenom_patient" />
+    </div>
+</div>
 
+    <div>
+        <label class="entree">Nom</label>
+        <input class="champs" type="text" v-model="item.nom_patient" />
+    </div>
+
+    </div>
     <!-- on vérifie que les patients n'est pas vide, et puis on boucle avec v-for sur un tableau d'objet "item" -->
 
-
-    <ul class="is-active"v-if="patients" id="example-1">
-        <li class="table"v-for="item in patients">
-            <router-link :to="{ name: 'patient-detail', params: { id: item.id_patient }}">{{ item.nom_patient }} {{ item.prenom_patient}}</router-link>
-        </li>
-    </ul>
-=======
-    <div class="rectangle">
-      <button class="add" to=/patients/patient-add v-on:click="sendModif" >Ajouter</button>
-      <div>
-          <label>Prénom</label>
-          <input type="text" v-model="item.prenom_patient" />
-      </div>
-      <div>
-          <label>Nom</label>
-          <input type="text" v-model="item.nom_patient" />
-      </div>
-
-
-      <button class="search">
-        <router-link class="search"  to=/patients/patient-detail>Rechercher</router-link>
-      </button>
-    <div/>
-
-{{message}}
-
-    <ul v-if="patients" id="example-1">
-        <li v-for="item in patients">
-            <router-link :to="{ name: 'patient-detail', params: { id: item.id_patient }}">{{ item.nom_patient }} {{ item.prenom_patient}}</router-link>
-        </li>
-    </ul>
-
+    <table>
+    <tr>
+        <th>Id Patient</th>
+        <th>Nom</th>
+        <th>Prenom</th>
+        <th>Adresse</th>
+        <th>Age</th>
+        <th>Actions</th>
+    </tr>
+<tbody v-if="patients">
+    
+    <tr v-for="item in patients">
+        <td>{{ item.id_patient}}</td>
+        <td>{{ item.nom_patient}}</td>
+        <td>{{ item.prenom_patient}}</td>
+        <td>{{ item.adresse}}</td>
+        <td>{{ item.age}}</td>
+                  
+        
+        <td>
+   
+        </td>
+    </tr>
+    
+</tbody>
+</table>   
 
   </div>
 `,
@@ -59,7 +65,8 @@ const PatientList = {
         return {
             patients:{},
             error: null,
-            loading:null
+            loading:null,
+            item:{}
         }
     },
     created() {
@@ -74,8 +81,32 @@ const PatientList = {
         fetchData() {
             axios.get('http://192.168.1.117/testphp/PIF_02/php/patient.php').then(response => {
                 this.patients= response.data.data;
-                console.log(response.data.data);
+                //console.log(response.data.data);
                 //alert("axiosok");
+            });
+        },
+        
+        sendModif() {
+            const params = new URLSearchParams();
+            params.append('prenom_patient', this.item.prenom_patient);
+            params.append('nom_patient', this.item.nom_patient);
+        
+
+
+            axios.post(' http://192.168.1.117/testphp/PIF_02/php/component_patient/insert_patient.php', params).then(response => {
+                console.log(this.item.nom_patient);
+              
+
+                this.item = response.data.data;
+                console.log(response);
+
+                if(response.data.error == 'false') {
+                    this.message = 'Patient ajouté';
+                }
+                else
+                {
+                    this.message = response.data.error_message;
+                }
             });
         }
     }
